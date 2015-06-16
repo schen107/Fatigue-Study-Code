5286575473f540d8a2153f6ccaed525edbf6745c
Steven Chen <Steven Chen@StevenChen-PC>
2015-06-16 00:16:15 -0400

    update final script

--- Components/FinalScript.m
+++ Components/FinalScript.m
@@ -137,6 +137,28 @@ TextScreen(window,'End of Phase 3','key');
 %% PHASE 4: CHOICE---------------------------------------------------------
 
 % For running only this phase
+% PsychDefaultSetup(2);screen=max(Screen('Screens'));
+% [window,windowRect]=PsychImaging('OpenWindow',screen,[0 0 0]);
+% 
+% sensor = 1;
+
+% sensor = analoginput('mcc'); %Default sample rate: 1000
+% chans=addchannel(sensor,0);
+% start(sensor);
+% 
+% pause(5); %Pausing to be sure that the sensor has time to equilibriate
+% baseline = getsample(sensor);
+% baseline = baseline(1);
+
+TextScreen(window,'Phase 4: Please wait for instructions','key');
+TextScreen(window,'GET READY',2);
+
+numChoiceTrials = 50;
+
+
+%% PHASE 5: FATIGUED CHOICE------------------------------------------------
+
+% For running only this phase
 PsychDefaultSetup(2);screen=max(Screen('Screens'));
 [window,windowRect]=PsychImaging('OpenWindow',screen,[0 0 0]);
 
@@ -150,10 +172,21 @@ sensor = 1;
 % baseline = getsample(sensor);
 % baseline = baseline(1);
 
-TextScreen(window,'Phase 4: Please wait for instructions','key');
+TextScreen(window,'Phase 5: Please wait for instructions','key');
 TextScreen(window,'GET READY',2);
 
+MVCFatiguePercent = 60;
+FailureThreshold = 30;
+numFatigueTrials = 3;
 
+for i = 1:numFatigueTrials
+    success = 0;
+    failure = 0;
+    while failure/success<3/7 && success+failure>
+
+%% PHASE 6: TRIAL SELECTION------------------------------------------------
+
+%% END
 
 KbStrokeWait;
 sca;
