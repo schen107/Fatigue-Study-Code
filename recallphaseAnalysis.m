7220f0720863de13a47f2e3b2f560b53031645be
Steven <Steven@10.252.102.179>
2015-07-30 15:15:03 -0400

    update recallphaseAnalysis

--- recallphaseAnalysis.m
+++ recallphaseAnalysis.m
@@ -6,55 +6,41 @@ rootpath = 'Y:\Fatigue Experiment\Pilot Data - 2'; %KKI Comp
 subjects = 0; %1 - multiple subjects, 0 - one subject;
 save = 0; %1 - save R^2 value and plot
 
-% For 1 subject
+if subjects == 1
+    SubjectID = {...
+        'KM_72015'...
+        'SM_71515'...
+        'JH_71515'...
+        'RL_71415'...
+        'ND_72115'...
+        'FO_72115'...
+        'SG_72215'...
+        'AG_72215'...
+    %     'patricktest_71715'...
+        };
+elseif subjects == 0
+    SubjectID = input('Enter Subject Identifier: ','s'); 
+end
 
-SubjectID=input('Enter Subject Identifier: ','s'); 
-SubjectDir = fullfile(rootpath,SubjectID);
-load(fullfile(SubjectDir,'RecallPhase'));
+for i = 1:size(SubjectID,2)
+    SubjectDir = char(fullfile(rootpath,SubjectID(i)));
+    load(fullfile(SubjectDir,'RecallPhase'));
 
-SumSquaredError = sum((RecallTrial(:,2)-RecallTrial(:,1)).^2);
-SampMean = mean(RecallTrial(:,2));
-SumSquaredMeanError = sum((RecallTrial(:,2)-SampMean).^2);
-rsquared = 1 - SumSquaredError/SumSquaredMeanError; 
+    SumSquaredError = sum((RecallTrial(:,2)-RecallTrial(:,1)).^2);
+    SampMean = mean(RecallTrial(:,2));
+    SumSquaredMeanError = sum((RecallTrial(:,2)-SampMean).^2);
+    rsquared = 1 - SumSquaredError/SumSquaredMeanError;
 
-figure(1);
-hold on;
-scatter(RecallTrial(:,1),RecallTrial(:,2),'*');
-line = linspace(0,100,100);
-plot(line,line);
-text(70,10,strjoin({'R^2 =',num2str(rsquared)}));
-axis([0 100 0 100]);
+    figure(i);
+    hold on;
+    scatter(RecallTrial(:,1),RecallTrial(:,2),'*');
+    line = linspace(0,100,100);
+    plot(line,line);
+    text(70,10,strjoin({'R^2 =',num2str(rsquared)}));
+    axis([0 100 0 100]);
+    if save == 1
+        saveas(gcf,fullfule(SubjectDir,'recallphasePlot');
+    end
+end
 
-
-% For multiple subjects
-% 
-% SubjectID = {...
-%     'KM_72015'...
-%     'SM_71515'...
-%     'JH_71515'...
-%     'RL_71415'...
-%     'ND_72115'...
-%     'FO_72115'...
-%     'SG_72215'...
-%     'AG_72215'...
-% %     'patricktest_71715'...
-%     };
-% 
-% for i = 1:size(SubjectID,2)
-%     SubjectDir = char(fullfile(rootpath,SubjectID(i)));
-%     load(fullfile(SubjectDir,'RecallPhase'));
-%     
-%     SumSquaredError = sum((RecallTrial(:,2)-RecallTrial(:,1)).^2);
-%     SampMean = mean(RecallTrial(:,2));
-%     SumSquaredMeanError = sum((RecallTrial(:,2)-SampMean).^2);
-%     rsquared = 1 - SumSquaredError/SumSquaredMeanError;
-%     
-%     figure(i);
-%     hold on;
-%     scatter(RecallTrial(:,1),RecallTrial(:,2),'*');
-%     line = linspace(0,100,100);
-%     plot(line,line);
-%     text(70,10,strjoin({'R^2 =',num2str(rsquared)}));
-%     axis([0 100 0 100]);
-% end
 
